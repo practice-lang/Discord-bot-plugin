968d0a3ca2de7d622dbea57ab54520214f7c8dd9
ttakkku <k2005118@gmail.com>
2019-03-23 01:16:32 +0900

    Updata filecreate

---
+++ com/ttakkku/listeners/filecreate.java
@@ -0,0 +1,22 @@
+package com.ttakkku.listeners;
+
+import java.io.*;
+
+
+public class filecreate  {
+    public void CreateNewfile() {
+        File filename = new File("plugins/Bot/token.yml");
+        File foler_Location = new File("plugins/Bot");
+        try {
+            if (!filename.exists()) {
+                foler_Location.mkdir();
+                filename.createNewFile();
+                BufferedWriter w = new BufferedWriter(new FileWriter(filename));
+                w.append("token: ");
+                w.flush();
+                w.close();
+            }
+        } catch (IOException localToException) {
+        }
+    }
+}
